4078ac476edfdee0a53fddf5a9a47e5e4102eb02
Alan Gimenez <gimenezalan00@gmail.com>
2021-08-29 14:16:42 -0300

    add navbar and footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <div className="container-fluid footer">
+            <div className="row">
+                <div className="col-12 col-md-4">
+                    <img src="https://picsum.photos/200/100" alt="imagen-x" />
+                </div>
+                <div className="col-12 col-md-8 footer_idiomas">
+                    <p>¿Deseas aprender otros idiomas? También tenemos para vos:</p>
+                    <div className="row">
+                        <a className="col-3 col-md-2" href="">Frances</a>
+                        <a className="col-3 col-md-2" href="">Italiano</a>
+                        <a className="col-3 col-md-2" href="">Portugues</a>
+                        <a className="col-3 col-md-2" href="">Ruso</a>
+                    </div>
+                </div>
+            </div>
+            <p className="footer_copyright">Copyright 2021. Hecho por Alan Gimenez, para quien lo necesite.</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <a href="">Home</a>
+            <a href="">Libros</a>
+            <a href="">Cursos</a>
+            <a href="">Prueba de nivel</a>
+            <a href="">Contacto</a>
+        </div>
+    )
+}
+
+export default Navbar;
